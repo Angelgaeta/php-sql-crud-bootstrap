d08b985a6f07990e3b93783bf4a303e4cac5459b
Angel R <93774378+Angelgaeta@users.noreply.github.com>
2022-12-18 16:30:27 +0100

    Add files via upload

--- create.php
+++ create.php
@@ -31,7 +31,7 @@
                 </div>
                 <div class="mb-3">
                     <label for="DateNaissance" class="form-label">Date de Naissance du stagiaire</label>
-                    <input type="text" class="form-control" id="DateNaissance" name="naissance" placeholder="YYYY-MM-DD" pattern="[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])">
+                    <input type="text" class="form-control" id="DateNaissance" name="naissance" placeholder="YYYY-MM-JJ" pattern="[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])">
                     <!-- <input type="text"  placeholder="YYYY-MM-DD" pattern="(?:19|20)(?:(?:[13579][26]|[02468][048])-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))|(?:[0-9]{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-8])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:29|30))|(?:(?:0[13578]|1[02])-31)))" class="form-control "  name="eventDate" id="" required autofocus autocomplete="nope"> -->
                 </div>
                 <div class="mb-3">
